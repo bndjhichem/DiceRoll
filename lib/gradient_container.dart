d6c171115536f01df8f92bd270fd80a9d98e33d2
Hichem <benabadji.hichem@gmail.com>
2023-08-21 12:32:31 +0100

    V1.0

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -27,24 +27,3 @@ class GradientContainer extends StatelessWidget {
     );
   }
 }
-
-//      child: Column(
-//          mainAxisSize: MainAxisSize.min,
-//          children: [
-//            Image.asset(
-//              'assets/images/dice-1.png',
-//              width: 200,
-//            ),
-//            const SizedBox(height: 20),
-//            TextButton(
-//              onPressed: rollDice,
-//             style: TextButton.styleFrom(
-//             foregroundColor: Colors.white,
-//           textStyle: const TextStyle(
-//           fontSize: 28,
-//         ),
-//        ),
-//         child: const Text('Roll Dice'),
-//       ),
-//      ],
-//      ),
